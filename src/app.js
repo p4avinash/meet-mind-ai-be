f91b11c09fd3202014b621a0399aa91b0cf72d98
Avinash <p4avinashmaurya@gmail.com>
2026-08-05 18:52:38 +0530

    cors error

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 import express from "express"
+import cors from "cors"
+import "dotenv/config"
 
 import errorHandler from "./middleware/errorHandler.js"
 import authRoutes from "./routes/auth.routes.js"
 
 const app = express()
+
+const allowedOrigins = [process.env.FRONTEND_URL].filter(Boolean)
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+)
 
 app.use(express.json())
 
